e166534a677b37f38ad7dc6529c8f5528a1b217d
vi <36610721+digitaimadness@users.noreply.github.com>
2022-11-02 21:33:20 +0300

    gpu utilization influences keyboard green leds (0%- blue; 100%-green!)

--- tuf-rgb-cpu.py
+++ tuf-rgb-cpu.py
@@ -1,5 +1,5 @@
 from time import sleep
-
+from os import system
 
 def writered(value):
     with open('/sys/devices/platform/faustus/kbbl/kbbl_red', "w") as f:
@@ -7,15 +7,19 @@ def writered(value):
 
 def writegreen(value=0): #static for now
     with open('/sys/devices/platform/faustus/kbbl/kbbl_green', "w") as f:
-        f.write(str(value))
+        f.write(str(hex(int(value))))
 
 def writeblue(value):
     with open('/sys/devices/platform/faustus/kbbl/kbbl_blue', "w") as f:
         f.write(str(hex(int(value))))
 
 def readscreenbrightness():
-    with open('/sys/class/backlight/amdgpu_bl0/brightness','r') as f:
+    with open('/sys/class/backlight/amdgpu_bl1/brightness','r') as f:
         return int((f.read()))
+
+def readgpuutilisation():
+    with open('/run/nvidiautilization','r') as f:
+        return f.readlines()[19]
 
 def commit():
     with open('/sys/devices/platform/faustus/kbbl/kbbl_set', "w") as f:
@@ -29,17 +33,22 @@ with open('/sys/devices/platform/faustus/kbbl/kbbl_speed', "w") as f:
 with open('/sys/devices/platform/faustus/kbbl/kbbl_flags', "w") as f:
     f.write("ff")
 lastidle = lasttotal = 0
+with open('/run/nvidiautilization', "w") as f:
+    f.write("")
 
 
 while True:
-    with open('/proc/stat') as cpu:
-        fields = [float(column) for column in cpu.readline().strip().split()[1:]]
+    system('/usr/bin/nvidia-smi -f=/run/nvidiautilization -q -d UTILIZATION,TEMPERATURE,MEMORY')
+    gpuutilisation = 2.55 * int(readgpuutilisation().strip(' Gpu:%\n'))
+    with open('/proc/stat') as f:
+        fields = [float(column) for column in f.readline().strip().split()[1:]]
     idle, total = fields[3], sum(fields)
     idledelta, totaldelta = idle - lastidle, total - lasttotal
     lastidle, lasttotal = idle, total
-    utilisation = 255 * (1.0 - idledelta / totaldelta)
+    cpuutilisation = 255 * (1.0 - idledelta / totaldelta)
     screenbrightnesscoef = readscreenbrightness() / 255
-    writered(screenbrightnesscoef * utilisation)
-    writeblue(screenbrightnesscoef * (255 - utilisation))
+    writered(screenbrightnesscoef * cpuutilisation)
+    writeblue(screenbrightnesscoef * (255 - cpuutilisation))
+    writegreen(screenbrightnesscoef * gpuutilisation)
     commit()
     sleep(0.1)
