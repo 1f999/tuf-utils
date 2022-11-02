bf2c6700bca89ec483cafc68a3e4d12d1197d269
vi <36610721+digitaimadness@users.noreply.github.com>
2022-11-02 17:48:10 +0300

    adjust keyboard brightness according to screen brightness + rewrite

--- tuf-rgb-cpu.py
+++ tuf-rgb-cpu.py
@@ -1,26 +1,45 @@
 from time import sleep
 
-with open('/sys/devices/platform/faustus/kbbl/kbbl_green', "w") as green:
-    green.write("00")
-with open('/sys/devices/platform/faustus/kbbl/kbbl_mode', "w") as mode:
-    mode.write("0")
-with open('/sys/devices/platform/faustus/kbbl/kbbl_speed', "w") as speed:
-    speed.write("0")
-with open('/sys/devices/platform/faustus/kbbl/kbbl_flags', "w") as flags:
-    flags.write("ff")
-last_idle = last_total = 0
+
+def writered(value):
+    with open('/sys/devices/platform/faustus/kbbl/kbbl_red', "w") as f:
+        f.write(str(hex(int(value))))
+
+def writegreen(value=0): #static for now
+    with open('/sys/devices/platform/faustus/kbbl/kbbl_green', "w") as f:
+        f.write(str(value))
+
+def writeblue(value):
+    with open('/sys/devices/platform/faustus/kbbl/kbbl_blue', "w") as f:
+        f.write(str(hex(int(value))))
+
+def readscreenbrightness():
+    with open('/sys/class/backlight/amdgpu_bl0/brightness','r') as f:
+        return int((f.read()))
+
+def commit():
+    with open('/sys/devices/platform/faustus/kbbl/kbbl_set', "w") as f:
+        f.write("2")
+
+
+with open('/sys/devices/platform/faustus/kbbl/kbbl_mode', "w") as f:
+    f.write("0")
+with open('/sys/devices/platform/faustus/kbbl/kbbl_speed', "w") as f:
+    f.write("0")
+with open('/sys/devices/platform/faustus/kbbl/kbbl_flags', "w") as f:
+    f.write("ff")
+lastidle = lasttotal = 0
+
 
 while True:
     with open('/proc/stat') as cpu:
         fields = [float(column) for column in cpu.readline().strip().split()[1:]]
     idle, total = fields[3], sum(fields)
-    idle_delta, total_delta = idle - last_idle, total - last_total
-    last_idle, last_total = idle, total
-    utilisation = 255 * (1.0 - idle_delta / total_delta)
-    with open('/sys/devices/platform/faustus/kbbl/kbbl_red', "w") as red:
-        red.write(str(hex(int((utilisation)))))
-    with open('/sys/devices/platform/faustus/kbbl/kbbl_blue', "w") as blue:
-        blue.write(str(hex(int((255 - utilisation)))))
-    with open('/sys/devices/platform/faustus/kbbl/kbbl_set', "w") as commit:
-        commit.write("2")
+    idledelta, totaldelta = idle - lastidle, total - lasttotal
+    lastidle, lasttotal = idle, total
+    utilisation = 255 * (1.0 - idledelta / totaldelta)
+    screenbrightnesscoef = readscreenbrightness() / 255
+    writered(screenbrightnesscoef * utilisation)
+    writeblue(screenbrightnesscoef * (255 - utilisation))
+    commit()
     sleep(0.1)
