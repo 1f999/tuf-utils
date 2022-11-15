5e80935da67377d90ba76c05d17f9e4f500a0a81
vi <36610721+digitaimadness@users.noreply.github.com>
2022-11-16 01:23:17 +0300

    merge scripts + optimizations

--- tuf-rgb-cpu.py
+++ tuf-rgb-cpu.py
@@ -1,61 +1,116 @@
 from time import sleep
-from os import system,path
+from os import system,path,chmod
+from ProcessMappingScanner import scanAllProcessesForMapping
 
-def writered(value):
-    with open('/sys/devices/platform/faustus/kbbl/kbbl_red', "w") as f:
-        f.write(str(hex(int(value))))
+def initled():
+    if path.isfile('/sys/class/backlight/amdgpu_bl0/brightness'):
+        brightnessdev = "0"
+    else:
+        brightnessdev = "1"
+    with open('/sys/devices/platform/faustus/kbbl/kbbl_mode', "w") as f:
+        f.write("0")
+    with open('/sys/devices/platform/faustus/kbbl/kbbl_speed', "w") as f:
+        f.write("0")
+    with open('/sys/devices/platform/faustus/kbbl/kbbl_flags', "w") as f:
+        f.write("ff")
+    with open('/run/nvidiautilization', "w") as f:
+        f.write("")
+    return brightnessdev
 
-def writegreen(value=0): #static for now
-    with open('/sys/devices/platform/faustus/kbbl/kbbl_green', "w") as f:
-        f.write(str(hex(int(value))))
+def initgamemode():
+    with open('/run/gamemode', 'w') as f:
+        f.write('0')
+    chmod('/run/gamemode',0o0777)
 
-def writeblue(value):
-    with open('/sys/devices/platform/faustus/kbbl/kbbl_blue', "w") as f:
-        f.write(str(hex(int(value))))
+def onbattery():
+    with open('/sys/class/power_supply/BAT1/status') as f:
+        if 'Discharging' in f.read():
+            return True
+        else:
+            return False
 
-def readscreenbrightness(dev):
+def readbrightness(dev):
     with open(dev,'r') as f:
         return int((f.read()))
 
 def readgpuutilization():
+    system('/usr/bin/nvidia-smi -f=/run/nvidiautilization -q -d UTILIZATION,TEMPERATURE,MEMORY')
     with open('/run/nvidiautilization','r') as f:
         return f.readlines()[19]
 
-def commit():
+def readgamemode(): #temporary solution - !requires setting these lines in /etc/gamemode.ini: start=echo "1" > /run/gamemode 
+                    #                                                                         end=echo "0" > /run/gamemode
+                    #!todo - develop gamemode controller service
+    with open('/run/gamemode', 'r') as f:
+        return int(f.read())
+
+def readthrottlemode():
+    with open('/sys/devices/platform/faustus/throttle_thermal_policy', 'r') as f:
+        return int(f.read())
+
+def setled(color,value):
+    with open('/sys/devices/platform/faustus/kbbl/kbbl_' + color, "w") as f:
+        f.write(str(hex(int(value))))
+
+def commitled():
     with open('/sys/devices/platform/faustus/kbbl/kbbl_set', "w") as f:
         f.write("2")
+    
+def setthrottlemode(mode):
+    with open('/sys/devices/platform/faustus/throttle_thermal_policy', 'w') as f:
+            f.write(str(mode))
 
 
-with open('/sys/devices/platform/faustus/kbbl/kbbl_mode', "w") as f:
-    f.write("0")
-with open('/sys/devices/platform/faustus/kbbl/kbbl_speed', "w") as f:
-    f.write("0")
-with open('/sys/devices/platform/faustus/kbbl/kbbl_flags', "w") as f:
-    f.write("ff")
-lastidle = lasttotal = 0
-with open('/run/nvidiautilization', "w") as f:
-    f.write("")
-
-
-while True:
-    system('/usr/bin/nvidia-smi -f=/run/nvidiautilization -q -d UTILIZATION,TEMPERATURE,MEMORY')
-    gpuutilization = 2.55 * int(readgpuutilization().strip(' Gpu:%\n'))
+def keyledcontrol(brightnessdev,lastidle,lasttotal,lastgpuutilization,n=2):
     with open('/proc/stat') as f:
         fields = [float(column) for column in f.readline().strip().split()[1:]]
-    idle, total = fields[3], sum(fields)
+    idle, total = fields[3], sum(fields) # calculating cpu utilization
     idledelta, totaldelta = idle - lastidle, total - lasttotal
     lastidle, lasttotal = idle, total
     cpuutilization = 255 * (1.0 - idledelta / totaldelta)
-    if path.isfile('/sys/class/backlight/amdgpu_bl0/brightness'):
-        screenbrightness = readscreenbrightness('/sys/class/backlight/amdgpu_bl0/brightness')
+    if n % 2 == 0:
+        gpuutilization = 2.55 * int(readgpuutilization().strip(' Gpu:%\n'))
     else:
-        screenbrightness = readscreenbrightness('/sys/class/backlight/amdgpu_bl1/brightness')
-    screenbrightnesscoef =  screenbrightness / 255
-    writered(screenbrightnesscoef * cpuutilization)
-    writegreen(screenbrightnesscoef * gpuutilization)
+        gpuutilization = lastgpuutilization
+    brightnesscoef = readbrightness('/sys/class/backlight/amdgpu_bl' + brightnessdev + '/brightness') / 255
+    setled("red", brightnesscoef * cpuutilization)
+    setled("green", brightnesscoef * gpuutilization)
     if cpuutilization >= gpuutilization:
-        writeblue(screenbrightnesscoef * (255 - cpuutilization))
+        setled("blue", brightnesscoef * (255 - cpuutilization) )
     else:
-        writeblue(screenbrightnesscoef * (255 - gpuutilization))
-    commit()
-    sleep(0.1)
+        setled("blue", brightnesscoef * (255 - gpuutilization) )
+    commitled()
+    return lastidle,lasttotal,gpuutilization
+
+
+def throttlecontrol():
+    throttlemode = readthrottlemode()
+    if throttlemode != 2 and onbattery():
+        setthrottlemode(2)
+    else:
+        gamemode = readgamemode()
+        if throttlemode != 1 and gamemode == 1:
+            setthrottlemode(1)
+        else:
+            if scanAllProcessesForMapping("clang") != {}:
+                setthrottlemode(1)
+            elif throttlemode != 0 and gamemode == 0:
+                setthrottlemode(0)
+
+
+def main(n=0,lastidle=0,lasttotal=0,lastgpuutilization=0):
+    initgamemode()
+    brightnessdev = initled()
+    while True:
+        n += 1
+        if n % 5 == 0:
+            throttlecontrol()
+        if onbattery():
+            if n % 10 == 0:
+                lastidle,lasttotal,lastgpuutilization = keyledcontrol(brightnessdev,lastidle,lasttotal,lastgpuutilization)    
+        else:
+                lastidle,lasttotal,lastgpuutilization = keyledcontrol(brightnessdev,lastidle,lasttotal,lastgpuutilization,n)    
+        sleep(0.1)
+
+
+main()
